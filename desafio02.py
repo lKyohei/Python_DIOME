2de66896e362989e22f1d4d3baab907ab67b545b
Eduardo Reis <102276374+lKyohei@users.noreply.github.com>
2023-09-02 23:49:16 -0300

    Add files via upload

---
+++ desafio02.py
@@ -0,0 +1,9 @@
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n=== Conta criada com sucesso! ===")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+
+    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
